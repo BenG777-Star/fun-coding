f1b91495593b3370eba1da552883079ae5088c51
Benjamin <benjamin.kitheko1@student.moringaschool.com>
2026-06-17 22:45:13 +0300

    Add index.js and primitiveDataType.js files

---
+++ JS/Intro/index.js
@@ -0,0 +1,10 @@
+let x = 15;
+            let y = 27;
+            let z = x + y;
+            console.log(z);
+
+
+let name="Benjamin";
+let greeting = 'Hello, ${name}!';
+console.log(greeting);
+            
---
+++ JS/Intro/primitiveDataType.js
@@ -0,0 +1,32 @@
+/*
+strings-> store sequence of characters
+3 ways of creating a string
+1.Double quote string
+2.Single quote string
+3.Back Ticks:<Template string>
+-->multine string
+*/
+/*
+special -> \t >tab
+newline -> \n
+single quote escape-> \'
+double quote escape-> \"
+*/
+let str1="Double \n quote I am Benjamin"//double quotes
+console.log(str1)
+let str2='Single Quote Iam benjamin'//Single quote
+console.log(str2)
+let str3='Back Ticks I am benjamin'//Back Ticks
+console.log(str3)
+let str4="23" //string or number:<String>
+console.log("23 is a string Don't")
+/*why do we multiply ways of representing a string*/
+let str5='Benjamin said "23 is a number"'
+//Let str5='Benjamin said'23 is a number''
+let str6=`
+Multi line string
+Multi line strings
+"" '' both single and double quotes
+Don't
+`
+let str7='Don\'t do this any more '
